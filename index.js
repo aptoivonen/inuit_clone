183ccf9b381598d84b8cad01cce9357b210fb900
Aleksi Toivonen <aptoivonen@mail.com>
2020-11-18 11:14:23 +0200

    Refactor hiding/showing code

--- index.js
+++ index.js
@@ -1,21 +1,61 @@
-const phoneInput = document.getElementById("phone");
-const phoneRecommendationText = document.getElementById("phone-recommendation");
-const phoneInfoText = document.getElementById("phone-info-text");
-const phoneVerifyTextMsg = document.getElementById("phone-verify-text-msg");
+const gebi = (id) => {
+  return document.getElementById(id);
+};
+
+const show = (element) => {
+  element.classList.remove("hide");
+};
+const hide = (element) => {
+  element.classList.add("hide");
+};
+const makeVisible = (element) => {
+  element.classList.remove("hidden");
+};
+const makeInvisible = (element) => {
+  element.classList.add("hidden");
+};
+const showRecommendationIcon = (element) => {
+  element.setAttribute("data-recommendation", "true");
+};
+const hideRecommendationIcon = (element) => {
+  element.removeAttribute("data-recommendation");
+};
+
+const emailInput = gebi("email");
+const emailErrorText = gebi("email-error-text");
+emailInput.addEventListener("blur", () => {
+  if (emailInput.value.trim()) {
+    if (!emailInput.validity.valid) makeVisible(emailErrorText);
+  }
+});
+emailInput.addEventListener("focus", () => {
+  makeInvisible(emailErrorText);
+});
+
+const phoneInput = gebi("phone");
+const phoneRecommendationText = gebi("phone-recommendation");
+const phoneInfoText = gebi("phone-info-text");
+const phoneVerifyTextMsg = gebi("phone-verify-text-msg");
+const phoneErrorText = gebi("phone-error-text");
 phoneInput.addEventListener("blur", () => {
   if (!phoneInput.value.trim()) {
-    phoneInput.setAttribute("data-recommendation", "true");
-    phoneRecommendationText.classList.remove("hide");
-    phoneInfoText.classList.add("hide");
-    phoneVerifyTextMsg.classList.add("hide");
+    showRecommendationIcon(phoneInput);
+    show(phoneRecommendationText);
+    hide(phoneInfoText);
+    hide(phoneVerifyTextMsg);
   } else {
-    phoneInput.removeAttribute("data-recommendation");
-    phoneRecommendationText.classList.add("hide");
-    phoneInfoText.classList.remove("hide");
+    hideRecommendationIcon(phoneInput);
+    hide(phoneRecommendationText);
+    if (phoneInput.validity.valid) show(phoneInfoText);
+    else {
+      hide(phoneInfoText);
+      makeVisible(phoneErrorText);
+    }
   }
 });
 phoneInput.addEventListener("focus", () => {
-  phoneRecommendationText.classList.add("hide");
-  phoneInfoText.classList.remove("hide");
-  phoneVerifyTextMsg.classList.remove("hide");
+  hide(phoneRecommendationText);
+  show(phoneInfoText);
+  show(phoneVerifyTextMsg);
+  makeInvisible(phoneErrorText);
 });
